332b9b2e0410daba705b6d5b5ec3161ebf4cfd6a
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-05 12:07:31 +0530

    Resolving tie break and continue the fliping till difference of 2 is acheived

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -11,10 +11,31 @@ do
 		((T++))
 	fi
 done
-
 if [ $H -eq $T ]
 then
 echo " it is tie "
+	while [ $(($H-$T)) -eq 0 ]
+	do
+	 	randomCheck=$((RANDOM%2))
+   	if [ $randomCheck -eq 1 ]
+   	then
+      	((H++))
+   	else
+      	((T++))
+   	fi
+		while [[ $(($H-$T)) -eq 1 || $(($T-$H)) -eq 1 ]]
+   	do
+      	randomCheck=$((RANDOM%2))
+      	if [ $randomCheck -eq 1 ]
+      	then
+         	((H++))
+      	else
+         	((T++))
+      	fi
+   	done
+	done
+echo "Heads: $H"
+echo "Tails : $T"
 elif [ $H -gt $T ]
 then
   echo "Heads won by $(($H-$T)) times more than Tails"
